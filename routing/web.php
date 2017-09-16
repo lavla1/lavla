8b4349927b9583a51a099fb985b56389791e26f1
Zack P <zack@zacklearns.com>
2017-09-16 11:33:41 -0600

    Create web.php

---
+++ routing/web.php
@@ -0,0 +1,8 @@
+<?php
+# This is where you can add pages to your website!  Go ahead and add them now!
+# Make something awesome!
+
+Route::get("/",function() {
+include $_SERVER["DOCUMENT_ROOT"]."index.php"
+})
+?>
