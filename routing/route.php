9901c54cc0a3ccf8a329c806a2d8c60d546df59c
Zack P <zack@zacklearns.com>
2017-09-16 11:36:33 -0600

    Create route.php

---
+++ routing/route.php
@@ -0,0 +1,9 @@
+<?php
+# This is where you can add services for your website. This is a little bit
+# harder to use then the web.php file.  Use that for web pages.  Don't get
+# fooled by the file name!
+
+Route::get("/users",function() {
+include $_SERVER["DOCUMENT_ROOT"]."users.php"
+})
+?>
