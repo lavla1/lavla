beebba5f7d33ef05a42c0e64bf582630879f36fd
Zack P <zack@zacklearns.com>
2017-09-16 12:12:38 -0600

    Allow logins.

--- users.php
+++ users.php
@@ -2,5 +2,29 @@
 $username = $users[1]
 $password = $users[2]
 
+$servername = $_SERVER["HTTP_HOST"];
+$username = "root";
+$password = "";
+  
+$conn = new mysqli($servername, $username, $password);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+
+$usr = "SELECT * FROM accs WHERE username=$username";
+$result = $conn->query($sql);
+  
+if (!$result->num_rows = 0) {
+  echo "This account was not found."
+} else {
+  while($row = $result->fetch_assoc()) {
+    if ($row["username"] == $username and $row["password"] == $password) {
+      echo "Good";
+    } else {
+      echo "Account information invalid."
+    }
+  }
+}
+  
+$conn->close();
 ?>
